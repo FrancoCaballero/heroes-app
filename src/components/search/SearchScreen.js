cf83bc9f9c194dedd1c3d3cd8a7d715e20bf84a2
Franco Caballero <franco.caballero.v@gmail.com>
2022-03-24 22:51:32 -0300

    feat: se agregan navegaciones y el componente de bnusqueda

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,5 +1,69 @@
-import React from "react";
+import { useMemo } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import queryString from "query-string";
+import { useForm } from "../../hooks/useForm";
+import { getHeroesByName } from "../../selectors/getHeroesByName";
+import { HeroCard } from "../hero/HeroCard";
 
 export const SearchScreen = () => {
-  return <div>SearchScreen</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const [{ searchText }, handleInputChange] = useForm({ searchText: q });
+
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`?q=${searchText}`);
+  };
+
+  return (
+    <>
+      <h1>Searches</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Search</h4>
+          <hr />
+
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="find hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Search...
+            </button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+
+          {q === "" ? (
+            <div className="alert alert-info"> Search hero</div>
+          ) : (
+            heroesFiltered.length === 0 && (
+              <div className="alert alert-danger"> No hay resultados: {q}</div>
+            )
+          )}
+
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
--- src/selectors/getHeroByPublisher.js
+++ src/selectors/getHeroByPublisher.js
@@ -1,6 +1,7 @@
 import { heroes } from "../data/heroes";
 
 export const getHeroesByPublisher = (publisher) => {
+  console.log("getHeroesByPublisher", publisher);
   const validPublishers = ["DC Comics", "Marvel Comics"];
   if (!validPublishers.includes(publisher)) {
     throw new Error(`Invalid publisher: ${publisher}`);
